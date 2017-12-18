3a678f65ad91c77532724a8bc4121e31a990e9ab
Perry Poon <plrthink@gmail.com>
2017-12-18 23:22:49 +0800

    ADD: @storybook/addon-info to provide story source as doc

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,10 @@
-import { configure } from '@storybook/react';
+import { configure, setAddon, addDecorator } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
 
 function loadStories() {
   require('../stories');
 }
+
+addDecorator((story, context) => withInfo('usage')(story)(context));
 
 configure(loadStories, module);
