4775eed26cabfffe834875e6f5921cf887dff50a
Perry Poon <plrthink@gmail.com>
2018-01-21 04:13:45 +0800

    generate js file for referencing styles

--- build/index.js
+++ build/index.js
@@ -1,17 +1,24 @@
 #!/usr/bin/env node
 
 const argv = require('yargs').argv
+const fs = require('fs')
+const path = require('path')
 const rollup = require('rollup')
 const getOptions = require('./rollup-options')
 
 getOptions(argv.entry, argv.dest, argv['use-modules'])
   .then(options => rollup.rollup(options))
   .then(function (bundle) {
-    bundle.write({
+    return bundle.write({
       format: 'es',
       dest: argv.dest,
       sourceMap: false
     })
+  })
+  .then(function () {
+    fs.writeFileSync(`${path.dirname(argv.dest)}/style/css.js`, 'import "./index.css"', 'utf8')
+    fs.writeFileSync(`${path.dirname(argv.dest)}/style/index.js`, 'import "./index.css"', 'utf8')
+    return
   })
   .catch(function (e) {
     console.log(e.message)
