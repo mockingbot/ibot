af818d432acb11dda9d9ab9091572d05a418ffc6
Chen Yijun <ethantw@me.com>
2018-04-24 13:56:26 +0800

    Add <SVG>

---
+++ components/svg/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { trimList } from '../util'
+
+import * as ICON_MAP from './icons'
+
+import './index.styl'
+
+function SVG({ name, icon = ICON_MAP[name], className, label, ...others }) {
+  if (!icon) return null
+
+  const [width, height, __html] = icon
+
+  const aria = label ? { 'aria-label': label } : { 'aria-hidden': true }
+
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={trimList(['svg-icon', name, className])}
+      width={width}
+      height={height}
+      {...{ ...aria, ...others }}
+      dangerouslySetInnerHTML={{ __html }}
+    />
+  )
+}
+
+SVG.propTypes = {
+  name: PropTypes.string,
+  icon: PropTypes.array,
+  className: PropTypes.string,
+  label: PropTypes.string,
+}
+
+export default SVG
