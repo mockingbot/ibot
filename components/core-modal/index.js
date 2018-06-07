c383e31ae9dccdeda140f9495db2dbfb29e06a84
Chen Yijun <ethantw@me.com>
2018-06-07 18:33:59 +0800

    Add <CoreModal>

---
+++ components/core-modal/index.js
@@ -0,0 +1,330 @@
+import React, { Fragment, PureComponent } from 'react'
+import { createPortal } from 'react-dom'
+import PropTypes from 'prop-types'
+import DocumentEvents from 'react-document-events'
+
+import { isEqual } from 'lodash'
+
+import { Button } from '../button'
+import SVG from '../svg'
+import Switch from '../switch'
+import Icon from '../icon'
+import { trimList, $, preparePortal } from '../util'
+
+import './index.styl'
+
+const OPEN_MODAL_STACK = []
+const stopPropagation = e => e.stopPropagation()
+
+const MODAL_ROOT_ID = 'IBOT_MODAL_ROOT'
+const MODAL_PORTAL_CLASS = 'CoreModalPortal'
+const CANT_SCROLL_CLASS = 'ibot-cant-scroll'
+
+const $body = document.body
+
+const $modalRoot = (
+  document.getElementById(MODAL_ROOT_ID)
+  || Object.assign(document.createElement('div'), { id: MODAL_ROOT_ID })
+)
+
+if (!$body.contains($modalRoot)) {
+  $body.appendChild($modalRoot)
+}
+
+const TYPE_CLASS_MAP = {
+  alert: 'AlertCoreModal',
+  form: 'FormCoreModal',
+  functional: 'FunctionalCoreModal',
+  display: 'DisplayCoreModal',
+}
+
+export default class CoreModal extends PureComponent {
+  static propTypes = {
+    isOpen: PropTypes.bool,
+    title: PropTypes.node,
+    children: PropTypes.node,
+
+    modal: PropTypes.node,
+    type: PropTypes.oneOf(['alert', 'form', 'functional', 'display']),
+
+    opener: PropTypes.node,
+    openerType: PropTypes.oneOf(['primary', 'regular', 'text', 'switch', 'custom', 'none']),
+
+    className: PropTypes.string,
+    maskClassName: PropTypes.string,
+    portalClassName: PropTypes.string,
+
+    onOpen: PropTypes.func.isRequired,
+    onClose: PropTypes.func.isRequired,
+    onToggle: PropTypes.func.isRequired,
+
+    canClose: PropTypes.bool,
+    canCloseOnClickMask: PropTypes.bool,
+    shouldCloseOnAction: PropTypes.bool,
+
+    canCloseOnEsc: PropTypes.bool,
+    canConfirmOnEnter: PropTypes.bool,
+
+    onConfirm: PropTypes.func,
+    confirmText: PropTypes.string,
+    isConfirmDisabled: PropTypes.bool,
+
+    onCancel: PropTypes.func,
+    isCancelDisabled: PropTypes.bool,
+    cancelText: PropTypes.string,
+  }
+
+  static defaultProps = {
+    openerType: 'none',
+    type: 'functional',
+
+    onOpen: () => null,
+    onClose: () => null,
+    onToggle: () => null,
+
+    canClose: true,
+    canCloseOnClickMask: true,
+    shouldCloseOnAction: true,
+    canCloseOnEsc: true,
+    canConfirmOnEnter: true,
+  }
+
+  state = {
+    prevProps: this.props,
+    isOpen: this.props.isOpen,
+    isVisible: this.props.isOpen,
+  }
+
+  static getDerivedStateFromProps(props, { prevProps }) {
+    if (!isEqual(prevProps, props)) {
+      return { prevProps: props, isOpen: props.isOpen }
+    }
+    return null
+  }
+
+  portal = preparePortal(
+    $modalRoot,
+    trimList([MODAL_PORTAL_CLASS, this.props.portalClassName]),
+  )
+
+  static getDerivedStateFromProps(props, { prevProps, isOpen }) {
+    if (!isEqual(prevProps, props)) {
+      const { isOpen: willBeOpen } = props
+
+      if (!isOpen && willBeOpen) {
+        return { isOpen: true, prevProps: props }
+      } else if (isOpen && !willBeOpen) {
+        return { isVisible: false, prevProps: props }
+      }
+      return { prevProps: props }
+    }
+
+    return null
+  }
+
+  componentDidMount() {
+    const { onOpen, onToggle } = this.props
+    const { isOpen } = this.state
+
+    if (isOpen) {
+      this.didOpen()
+    }
+
+    window.addEventListener('resize', this.positionY)
+  }
+
+  componentDidUpdate(_, { isOpen: wasOpen }) {
+    const { isOpen } = this.state
+
+    if (!wasOpen && isOpen) {
+      setTimeout(() => this.setState(
+        { isVisible: true },
+        this.didOpen,
+      ))
+    } else if (wasOpen && !isOpen) {
+      this.didClose()
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.portal) this.portal.remove()
+    $body.classList.remove(CANT_SCROLL_CLASS)
+
+    window.removeEventListener('resize', this.positionY)
+  }
+
+  didOpen = () => {
+    const { onOpen, onToggle } = this.props
+
+    OPEN_MODAL_STACK.unshift(this)
+
+    this.positionY()
+    $body.classList.add(CANT_SCROLL_CLASS)
+
+    onOpen()
+    onToggle(true)
+  }
+
+  didClose = () => {
+    const { onClose, onToggle } = this.props
+
+    // Remove from the stack in the next round:
+    const idx = OPEN_MODAL_STACK.indexOf(this)
+    setTimeout(() => OPEN_MODAL_STACK.splice(idx, 1))
+
+    $body.classList.remove(CANT_SCROLL_CLASS)
+
+    onClose()
+    onToggle(false)
+  }
+
+  onTransitionEnd = ({ target }) => {
+    const { isVisible } = this.state
+
+    if (!isVisible && target.matches('.CoreModalMask')) {
+      this.setState({ isOpen: false })
+    }
+  }
+
+  open = () => this.setState({ isOpen: true })
+  close = () => this.setState({ isVisible: false })
+
+  toggle = (willBeOpen = !this.state.isOpen) => (
+    willBeOpen ? this.open() : this.close()
+  )
+
+  positionY = () => setTimeout(() => {
+    const { type } = this.props
+    const $modal = $('.CoreModal', this.portal)
+
+    if (!$modal || type === 'alert') return
+
+    const { innerHeight: vh } = window
+    const { offsetHeight: h } = $modal
+
+    const action = (vh <= h || ((vh - h)/2) < (vh * .2)) ? 'add' : 'remove'
+    $modal.classList[action]('is-v-centered')
+  })
+
+  onClickMask = e => {
+    stopPropagation(e)
+
+    const { canClose, canCloseOnClickMask } = this.props
+    const isSelectMenuOpen = !!$('#IBOT_SELECT_MENU_ROOT .SelectMenu.is-open')
+
+    if (canClose && canCloseOnClickMask && !isSelectMenuOpen) {
+      this.close()
+    }
+  }
+
+  onKeyDown = ({ key, target: $elmt }) => {
+    const {
+      type,
+      canClose, canCloseOnEsc,
+      canConfirmOnEnter,
+      onConfirm,
+    } = this.props
+
+    const { isOpen } = this.state
+    const isSelectMenuOpen = !!$('#IBOT_SELECT_MENU_ROOT .SelectMenu.is-open')
+
+    if (
+      key === 'Escape'
+
+      // Not focus on form elements:
+      && !$elmt.matches('input, textarea, select') && !isSelectMenuOpen
+
+      // Current modal is open and can close via esc:
+      && isOpen && canClose && canCloseOnEsc && !isSelectMenuOpen
+
+      // Only work on the toppest modal:
+      && this === OPEN_MODAL_STACK[0]
+    ) {
+      this.close()
+    }
+  }
+
+  render() {
+    return this.opener
+  }
+
+  get opener() {
+    const { opener, openerType } = this.props
+    const { isOpen } = this.state
+
+    const { modal } = this
+
+    return (
+      openerType === 'none'
+      ? modal
+
+      : openerType === 'custom'
+      ? (
+        opener
+        ? <span onClick={this.toggle}>
+            { opener }
+            { modal }
+          </span>
+        : modal
+      )
+
+      : openerType === 'switch'
+      ? <Switch isChecked={isOpen} onChange={this.toggle}>
+          { modal }
+        </Switch>
+
+      : <Button type={openerType} onClick={this.open}>
+          { opener }
+          { modal }
+        </Button>
+    )
+  }
+
+  get modal() {
+    return createPortal(this.modalDOM, this.portal)
+  }
+
+  get modalDOM() {
+    const {
+      type,
+      maskClassName,
+      className,
+
+      children,
+
+      canClose, canCloseOnClickMask,
+    } = this.props
+
+    const { isOpen, isVisible } = this.state
+
+    return isOpen && (
+      <div
+        className={trimList([
+          'CoreModalMask',
+          maskClassName,
+          isVisible && 'is-open',
+          canClose && canCloseOnClickMask ? 'can-close' : 'cant-close',
+        ])}
+        onClick={this.onClickMask}
+        onTransitionEnd={this.onTransitionEnd}
+      >
+        {/* Close button */}
+        { canClose && (
+          <Button type="text" className="close-btn" onClick={this.close}>
+            <SVG name="close" label="Close the Modal" />
+          </Button>
+        )}
+
+        <div
+          className={trimList(['CoreModal', className, TYPE_CLASS_MAP[type]])}
+          onTransitionEnd={stopPropagation}
+          onClick={stopPropagation}
+        >
+          { children }
+        </div>
+
+        <DocumentEvents onKeyDown={this.onKeyDown} />
+      </div>
+    )
+  }
+}
---
+++ stories/core-modal.js
@@ -0,0 +1,531 @@
+import React, { PureComponent } from 'react'
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+
+import {
+  Root, Icon, Switch, CoreModal as Modal, text,
+  FormLabel, FormEntry,
+  InputNumber, Textarea, Input,
+  RadioGroup, CheckGroup,
+  Select,
+  WidgetName,
+} from '../components'
+
+storiesOf('Core Modal', module)
+.add('Default', () => (
+  <Root>
+    <Modal isOpen={true} onClose={action('Modal closed')} />
+  </Root>
+))
+.add('Openers', () => (
+  <Root>
+    <p>
+      <Modal
+        openerType="switch"
+
+        isOpen={true}
+        title="Modal’s Title"
+
+        onToggle={action('Modal toggled, `isOpen`')}
+      >
+        Modal opened with <Switch />
+      </Modal>
+    </p>
+
+    <p>
+      <Modal openerType="custom" /* Shall not display */ />
+
+      <Modal
+        openerType="custom" opener="Custom Opener"
+        type="form"
+        onToggle={action('Modal toggled, `isOpen`')}
+      />
+    </p>
+
+    <p>
+      <style scoped>
+      {`p button { margin-right: 1em; }`}
+      {`p button .icon { font-size: 1.2em; vertical-align: -.1em }`}
+      </style>
+
+      <Modal
+        opener="Open a Modal"
+        openerType="primary"
+
+        isOpen={false}
+        title="Modal’s Title"
+
+        onToggle={action('Modal toggled, `isOpen`')}
+      >
+        Modal opened with a button
+      </Modal>
+
+      <Modal
+        opener="Open a Modal"
+        openerType="regular"
+
+        isOpen={false}
+        title="Modal’s Title"
+
+        onToggle={action('Modal toggled, `isOpen`')}
+      >
+        Modal opened with a button
+      </Modal>
+
+      <Modal
+        opener="Open a Modal"
+        openerType="text"
+
+        isOpen={false}
+        title="Modal’s Title"
+
+        onToggle={action('Modal toggled, `isOpen`')}
+      >
+        Modal opened with a button
+      </Modal>
+
+      <Modal
+        isOpen={false}
+        opener={[<Icon key="icon" name="share" />, 'Open a Modal']}
+        openerType="text"
+        title="Modal’s Title"
+
+        onToggle={action('Modal toggled, `isOpen`')}
+      >
+        Modal opened with a button
+      </Modal>
+    </p>
+
+    <p>
+      Opening a modal in a modal:{' '}
+      <Modal
+        openerType="switch"
+
+        isOpen={false}
+        title="Modal’s Title"
+
+        onToggle={action('Modal toggled, `isOpen`')}
+      >
+        Give me an alert:{' '}
+        <Modal
+          openerType="switch" type="alert" title="Yay!"
+          onToggle={action('MiM toggled, `isOpen`')}
+        >
+          Modal in a modal is open!
+        </Modal>
+      </Modal>
+    </p>
+  </Root>
+))
+.add('Advanced', () => (
+  <Root>
+    <style>
+    {`#root .FormEntry { width: 22em; }`}
+    {`h2 + .FormEntry { margin-top: -1em; }`}
+    {`.AlertCoreModal p, .FormCoreModal p { margin: .5em 0; }`}
+    {`.FormEntry > .key { flex-basis: 15em; }`}
+    {`button.text .icon { margin-right: .25em; font-size: 1.1em; vertical-align: -.1em; }`}
+    </style>
+
+    <h2>Alert</h2>
+    <FormLabel name="Default">
+      <Modal
+        opener="Alert"
+        openerType="text"
+
+        type="alert"
+        title="Warning"
+        onToggle={action('Modal toggled, `isOpen`')}
+      >
+        <p>
+          An alert automatically comes with a confirm button,
+          whether you provide the <code>onConfirm</code> callback or not.
+        </p>
+      </Modal>
+    </FormLabel>
+
+
+    <FormLabel name="Action Callbacks">
+      <Modal
+        openerType="switch"
+
+        type="alert"
+        title="Warning"
+
+        onConfirm={action('Confirmed')}
+        confirmText="Try Me!"
+
+        onCancel={action('Cancelled')}
+        cancelText="Nope!"
+        onToggle={action('Modal toggled, `isOpen`')}
+      >
+        <p>Something serious just happened!</p>
+        <p>P.S. Try the cancel and confirm buttons below.</p>
+      </Modal>
+    </FormLabel>
+
+    <FormLabel name="Not closing after actions">
+      <Modal
+        openerType="switch"
+
+        type="alert"
+        title="Warning"
+
+        onConfirm={action('Confirmed')}
+        onCancel={action('Cancelled')}
+        shouldCloseOnAction={false}
+        onToggle={action('Modal toggled, `isOpen`')}
+      >
+        <p>Try the cancel and confirm buttons below.</p>
+        <p>You get to decide whether to close the modal after actions or not.</p>
+      </Modal>
+    </FormLabel>
+
+    <FormLabel name={<span>Can’t close <strong> (serious)</strong></span>}>
+      <Modal
+        openerType="switch"
+
+        type="alert"
+        title="Inclosable Modal"
+        maskClassName="cant-close-modal-mask"
+
+        canClose={false}
+
+        onOpen={() => Object.assign(window, {
+          ccm_interval: setInterval(() => {
+            try {
+              const $mask = document.querySelector('.cant-close-modal-mask')
+              const $countdown = $mask.querySelector('.countdown')
+              let countdown = parseInt($countdown.innerHTML)
+
+              countdown--
+              $countdown.innerHTML = `${countdown}s`
+
+              if (countdown <= 0) {
+                clearInterval(window.ccm_interval)
+
+                /**
+                 * **NOTE** Removing the `is-open` class is an anti-pattern to
+                 * close a modal, you should instead alter the `isOpen` property
+                 * of the component in its stateful parent for such purpose.
+                 */
+                $mask.classList.remove('is-open')
+              }
+            } catch (e) {
+              clearInterval(window.ccm_interval)
+            }
+          }, 1000)
+        })}
+
+        onToggle={action('Modal toggled, `isOpen`')}
+        onClose={action('Successfully closed an inclosable modal')}
+      >
+        <p>This modal cannot be closed manually.</p>
+        <p>
+          Provide an <code>onOpen</code> callback and set a timeout to
+          close the modal by altering its <code>isOpen</code> property.
+        </p>
+
+        <p>
+          This modal will be closed in {' '}
+          <span className="countdown" style={{ color: '#eb5648' }}>7s</span>
+          {' '} automatically.
+        </p>
+      </Modal>
+    </FormLabel>
+
+    <FormLabel name="Can’t close via clicking mask">
+      <Modal
+        openerType="switch"
+
+        type="alert"
+        title="Warning"
+
+        canCloseOnClickMask={false}
+        onToggle={action('Modal toggle, `isOpen`')}
+      >
+        <p>
+          This modal cannot be closed through clicking mask.
+        </p>
+
+        <p>P.S. Other regular modals can.</p>
+      </Modal>
+    </FormLabel>
+
+    <FormLabel name="Can’t close/confirm with key-pressing">
+      <Modal
+        openerType="switch"
+
+        type="alert"
+        title="Warning"
+
+        canCloseOnEsc={false}
+        canConfirmOnEnter={false}
+
+        onToggle={action('Modal toggled, `isOpen`')}
+      >
+        <p>
+          This modal cannot be closed via pressing <kbd>Esc</kbd> key
+          and cannot be confirmed via pressing <kbd>Enter</kbd> key.
+        </p>
+
+        <p>P.S. Other regular modals can.</p>
+      </Modal>
+    </FormLabel>
+
+
+    <h2>Form</h2>
+    <NewMasterFormLabel />
+
+    <FormLabel name="Transfer Screen(s)">
+      <Modal
+        opener={<Icon name="exchange" />}
+        openerType="text"
+
+        type="form"
+        title="Transfer Screen(s)"
+        onConfirm={action('Confirmed')}
+        onCancel={action('Cancelled')}
+        onToggle={action('Modal toggled, `isOpen`')}
+      >
+        <p style={{ margin: '0 0 1.5em' }}>转移页面「<WidgetName name="2017年末重要促销页面，禁止外流！！" />」到其他位置。</p>
+        <FormLabel name="目标项目">
+          <Select
+            placeholder="选择一个项目"
+            optionList={[
+              ['我的项目', '私ノ友達', '双十一的特价活动超强報价页面，十月底最终版'],
+              ['洋基队', 'InstaYankies', 'New York New York', 'Manhattan Project'],
+              ['巨人队', 'Taller Men', 'Shorter Giants'],
+              ['红襪队', '一个很長很長又臭又長很長很長又臭又長很長很長又臭又長又長又臭又長又臭又臭又長的项目名字'],
+            ]}
+          />
+        </FormLabel>
+      </Modal>
+    </FormLabel>
+
+    <FormLabel name="Y-position">
+      <Modal
+        openerType="switch"
+
+        type="form"
+        title="Demo for Modals"
+        onToggle={action('Modal toggled, `isOpen`')}
+      >
+        <p>
+          Generally, modals will be positioned at 20vh vertically.
+        </p>
+      </Modal>
+    </FormLabel>
+
+    <FormLabel name="Y-position for slightly-longer modals">
+      <Modal
+        openerType="switch"
+
+        type="form"
+        title="Demo for Slightly-longer Modals"
+        className="s-long-modal"
+        onToggle={action('Modal toggled, `isOpen`')}
+      >
+        <style>{`.s-long-modal { height: 500px; }`}</style>
+        <p>
+          Should the Y position of a slightly-longer modal (except alert)
+          is smaller than 20vh while it’s positioned vertically-centered,
+          the modal is positioned vertically-centered.
+        </p>
+      </Modal>
+    </FormLabel>
+
+    <FormLabel name="Y-position for long modals">
+      <Modal
+        openerType="switch"
+
+        type="form"
+        title="Demo for Long Modals"
+        className="long-modal"
+        onToggle={action('Modal toggled, `isOpen`')}
+      >
+        <style>{`.long-modal { height: 1200px; }`}</style>
+        <p>
+          Long modals except for alerts will be positioned vertically
+          at 50px and there’ll be a 50px bottom margin for visual reason.
+        </p>
+      </Modal>
+    </FormLabel>
+
+    <h2>Functional</h2>
+    <FormLabel name="Share">
+      <Modal
+        opener={[<Icon key="icon" name="share" />, 'Share']}
+        openerType="text"
+
+        title="Share"
+        className="share-modal"
+
+        onToggle={action('Modal toggled, `isOpen`')}
+      >
+        <style>
+        {`.share-modal .FormEntry > .key { flex-basis: 10em; }`}
+        {`.share-modal .FormEntry > .val label { width: 100%; }`}
+        {`.share-modal .Textarea { width: 100%; }`}
+        </style>
+
+        <p>Embed the app in a website or blog by the code below:</p>
+        <Textarea
+          readOnly
+          value='<iframe src="https://modao.cc/app/123/embed" width="488" height="900" allowTransparency="true" frameborder="0"></iframe>'
+          onClick={() => document.querySelector('.share-modal textarea').select()}
+          style={{ width: '100%', height: '5em' }}
+        />
+
+        <FormEntry name="Access">
+          <RadioGroup
+            optionList={[
+              'Only for Collaborators',
+              {
+                label: (
+                  <span>
+                    Anyone with the URL and optional password:
+                    {' '}
+                    <Input key="input" type="password" />
+                  </span>
+                ),
+                value: 'pwd',
+              },
+            ]}
+            value="pwd"
+          />
+        </FormEntry>
+
+        <FormEntry name="Preview Settings">
+          <CheckGroup
+            optionList={[
+              'Highlight clickable areas on the screens.',
+              'Play the app directly without showing install instructions.',
+            ]}
+            valueList={['Highlight clickable areas on the screens.']}
+          />
+        </FormEntry>
+
+      </Modal>
+    </FormLabel>
+
+    <h2>Display</h2>
+    <FormLabel name="Shortcuts">
+      <Modal
+        opener={[<Icon key="icon" name="keyboard" />, 'Shortcuts']}
+        openerType="text"
+
+        type="display"
+        title="Shortcuts"
+        onToggle={action('Modal toggled, `isOpen`')}
+      >
+        <p>
+          Display modals are designed to display information that
+          needs larger spaces, i.e. list of shortcuts.
+        </p>
+      </Modal>
+    </FormLabel>
+ </Root>
+))
+
+
+class NewMasterFormLabel extends PureComponent {
+  state = { isOpen: false }
+
+  onToggle = isOpen => this.setState({ isOpen })
+
+  render() {
+    const { isOpen } = this.state
+
+    return (
+      <FormLabel name="Add New Master">
+        <NewMasterModal isOpen={isOpen} onToggle={this.onToggle} />
+      </FormLabel>
+    )
+  }
+}
+
+class NewMasterModal extends PureComponent {
+  state = {
+    name: '',
+    access: 'public',
+    w: 300,
+    h: 50,
+  }
+
+  onChangeName = name => this.setState({ name })
+  onToggleAccess = ({ value: access }) => this.setState({ access })
+
+  onChangeW = w => this.setState({ w })
+  onChangeH = h => this.setState({ h })
+
+  render() {
+    const { isOpen, onToggle } = this.props
+    const { name, access, w, h } = this.state
+
+    return (
+      <Modal
+        openerType="text"
+        opener={<Icon name="plus" />}
+        className="master-modal"
+
+        isOpen={isOpen}
+
+        type="form"
+        title="New Master"
+        onConfirm={action('Confirmed')}
+        onCancel={action('Cancelled')}
+        onToggle={action('Modal toggled, `isOpen`')}
+
+        onToggle={onToggle}
+      >
+        <p>All modals will try to focus on the first input element once opened.</p>
+
+        <FormLabel name="Name">
+          <Input
+            placeholder="Name the new master"
+            value={name}
+            onChange={this.onChangeName}
+          />
+        </FormLabel>
+
+        <FormEntry name="Access">
+          <RadioGroup
+            optionList={[
+              { label: 'Public', value: 'public' },
+              { label: 'Private', value: 'private' },
+            ]}
+            value={access}
+            onChange={this.onToggleAccess}
+          />
+        </FormEntry>
+
+        <FormEntry name="Size">
+          <style>
+          {`
+            .master-modal .FormEntry > .val > input.regular[type=number] {
+              display: inline-block;
+              width: 6em;
+            }
+
+            .master-modal .size {
+              display: flex;
+              align-items: baseline;
+            }
+            .master-modal .times {
+              width: 3em;
+              text-align: center;
+            }
+          `}
+          </style>
+
+          <div className="size">
+            <InputNumber value={w} onChange={this.onChangeW} />
+            <span className="times">&times;</span>
+            <InputNumber value={h} onChange={this.onChangeH} />
+          </div>
+        </FormEntry>
+      </Modal>
+    )
+  }
+}
