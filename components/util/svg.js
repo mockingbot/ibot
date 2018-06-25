8016397c91f8af3efcbba2bbc6537aa84e6ca75d
Chen Yijun <ethantw@me.com>
2018-06-25 16:30:29 +0800

    More specific style selector for arrows of <Dropdown>

--- components/util/svg.js
+++ components/util/svg.js
@@ -1,4 +1,4 @@
 export const INPUT_ARROW = `<svg width="6" height="4" viewBox="0 0 6 4"><path d="M3 0l3 4H0"></path></svg>`
-export const DROPDOWN_ARROW = `<svg width="8" height="3" viewBox="0 0 8 3"><path d="M4.654.233L8 3H0L3.383.23c.37-.303.902-.302 1.27.003z" /></svg>`
+export const DROPDOWN_ARROW = `<svg viewBox="0 0 8 3"><path d="M4.654.233L8 3H0L3.383.23c.37-.303.902-.302 1.27.003z" /></svg>`
 export const GUIDE_ARROW = `<svg width="27" height="8"><path d="M15.069 6.66a3.27 3.27 0 0 1-3.13 0L0 0h27.008l-11.94 6.66z" /></svg>`
 export const CLOSE_FILLED = `<svg width="16" height="16"><path d="M8 6.945L6.218 5.163a.688.688 0 0 0-.973.972l1.783 1.782L5.245 9.7a.688.688 0 0 0 .973.972L8 8.89l1.782 1.782a.688.688 0 0 0 .973-.972L8.972 7.917l1.783-1.782a.688.688 0 0 0-.973-.972L8 6.945zm-5.185 6.24a7.333 7.333 0 1 1 10.37-10.37 7.333 7.333 0 0 1-10.37 10.37z"/></svg>`
