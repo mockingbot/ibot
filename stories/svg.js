97d4aa149bd9cd0244d7792370941f5f9fa42556
Chen Yijun <ethantw@me.com>
2018-07-05 18:36:39 +0800

    Add SVG icons

--- stories/svg.js
+++ stories/svg.js
@@ -6,10 +6,58 @@ import { Root, SVG } from '../components'
 storiesOf('SVG', module)
 .add('default', () => (
   <Root>
-    <h2>Preview</h2>
+    <style>
+      {`.svg-icon { margin-right: .25em; font-size: 1rem; color: #8D9EA7; }`}
+      {`.svg-icon .fore { fill: #FF7100; }`}
+      {`.dir .svg-icon { width: 1em; height: 1em; }`}
+    </style>
 
-    <SVG name="daynight" />
-    <SVG name="exit" />
-    <SVG name="fullscreen" />
+    <h2>Directions</h2>
+    <p className="dir">
+    {
+      [
+        'angle_bracket_up', 'angle_bracket_down', 'angle_bracket_left', 'angle_bracket_right',
+        'triangle_up', 'triangle_down',
+      ]
+      .map(it => <SVG key={it} name={it} />)
+    }
+    </p>
+
+    <h2>General</h2>
+    <style>{`.svg-close .fore { fill: #fff; }`}</style>
+    <p>
+    {
+      ['alarm', 'close', 'ellipsis']
+      .map(it => <SVG key={it} className={`svg-${it}`} name={it} />)
+    }
+    </p>
+
+    <h2>Math</h2>
+    <p>
+    {
+      ['plus', 'times']
+      .map(it => <SVG key={it} name={it} />)
+    }
+    </p>
+
+    <h2>Actions</h2>
+    <p>
+    {
+      [
+        'pin', 'duplicate', 'move', 'trash',
+        'code', 'play', 'share', 'workflow', 'setting',
+        'loading', 'scale_enlarge', 'scale_reduce',
+      ]
+      .map(it => <SVG key={it} name={it} />)
+    }
+    </p>
+
+    <h2>Preview</h2>
+    <p>
+    {
+      ['daynight', 'exit', 'fullscreen']
+      .map(it => <SVG key={it} name={it} />)
+    }
+    </p>
   </Root>
 ))
