afe7210a28b203604461d08d301f49ab6acd28ca
dr-js <dr@dr.run>
2019-06-13 15:29:51 +0800

    Upgrade package versions & sort config file

---
+++ scripts/copy-resource-file.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+/* eslint-disable no-console */
+
+const { fromRoot, mkdirP, copyFileSync } = require('./function')
+
+const copyList = [
+  // components/icon/fonts/
+  [ 'node_modules/mb-icons/mb/fonts/mb-icons.woff', 'components/icon/fonts/mb-icons.woff' ],
+  [ 'node_modules/mb-icons/mb/fonts/mb-icons.ttf', 'components/icon/fonts/mb-icons.ttf' ],
+  [ 'node_modules/mb-icons/mb/fonts/mb-icons.svg', 'components/icon/fonts/mb-icons.svg' ],
+  [ 'node_modules/mb-icons/dora/fonts/dora.woff', 'components/icon/fonts/dora.woff' ],
+  [ 'node_modules/mb-icons/dora/fonts/dora.ttf', 'components/icon/fonts/dora.ttf' ],
+  [ 'node_modules/mb-icons/dora/fonts/dora.svg', 'components/icon/fonts/dora.svg' ],
+
+  // stories/json/
+  [ 'node_modules/mb-icons/dora/icon-list.json', 'stories/json/dora.json' ],
+  [ 'node_modules/mb-icons/dora/duo-list.json', 'stories/json/duo.json' ],
+  [ 'node_modules/mb-icons/mb/icon-list.json', 'stories/json/mb.json' ],
+]
+
+mkdirP(fromRoot('stories/json/'))
+for (const [ fromFile, toFile ] of copyList) {
+  copyFileSync(fromRoot(fromFile), fromRoot(toFile))
+}
--- scripts/icon-storybook.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p ./stories/json
-cp ./node_modules/mb-icons/dora/icon-list.json ./stories/json/dora.json
-cp ./node_modules/mb-icons/dora/duo-list.json ./stories/json/duo.json
-cp ./node_modules/mb-icons/mb/icon-list.json ./stories/json/mb.json
